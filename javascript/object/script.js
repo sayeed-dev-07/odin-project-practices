71bc7d8d9555785286e7da31e455ec6cb04deda2
Sayeed <expsayeedshorif@gmail.com>
2025-01-10 21:07:18 +0600

    added some new exercise to the exerice sheet

--- javascript/object/script.js
+++ javascript/object/script.js
@@ -64,19 +64,25 @@ gameOver(playerOne);
 
 // oop construction lesson from odin project
 
-function playerMaker(name, marker) {
+function Player(name, marker) {
     this.name = name;
     this.marker = marker;
     this.sayName = function() {
         console.log(`My name is ${this.name}`);
     }
 }
-const player1 = new playerMaker('alex', 'X');
+const player1 = new Player('alex', 'X');
+const player2 = new Player('shorif', 'O');
 console.log(player1.name);
 console.log(player1.marker);
 player1.sayName();
 
 
+Player.prototype.sayMyMarker = function() {
+    console.log(`My marker is ${this.marker}`);
+};
+player1.sayMyMarker();
+player2.sayMyMarker();
 // object exerices from odin project
 
 let bookMaker = function book(title, author, read, pages) {
@@ -95,3 +101,78 @@ let bookMaker = function book(title, author, read, pages) {
 
 const book1 = new bookMaker('The Hobbit', 'J.R.R. Tolkien', false, 295);
 book1.info();
+
+// prototypal inheritance
+
+function Person(name) {
+    this.name = name;
+}
+
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}`);
+}
+
+function Student(name, level) {
+    this.name = name;
+    this.level = level;
+}
+
+Student.prototype.className = function(){
+    console.log(`I'm a ${this.level} student`);
+}
+Object.setPrototypeOf(Student.prototype, Person.prototype);
+
+let student1 = new Student('sayeed', '8th');
+let student2 = new Student('shorif', '10th');
+
+student1.className();
+student1.greet();
+
+student2.className();
+student2.greet();
+
+
+let array = new Array();
+
+// constructor method example
+
+function Hero(name, level){
+    this.name = name;
+    this.level = level;
+}
+
+let hero1 = new Hero('sayeed', '9th')
+console.log(hero1);
+
+Hero.prototype.greet = function(){
+    return `Hello im ${this.name} and im ${this.level}'s hero!` 
+}
+
+console.log(hero1.greet());
+
+function warrior(name, level, weapon){
+    Hero.call(this, name, level);
+    this.weapon = weapon;
+
+}
+warrior.prototype.attack = function(){
+    return `The warrior attacks with ${this.weapon}`
+}
+
+function healer(name, level, portion){
+ Hero.call(this, name, level);
+ this.portion = portion;
+}
+
+healer.prototype.heal = function(){
+    return `The healer heals with ${this.portion} portion`
+}
+
+const hero3 = new warrior('Bjorn', 1, 'axe');
+const hero4 = new healer('Kanin', 1, 'cure');
+
+console.log(hero3);
+console.log(hero4);
+
+console.log(hero3.attack());
+console.log(hero4.heal());
--- javascript/object/tempCodeRunnerFile.js
+++ javascript/object/tempCodeRunnerFile.js
@@ -1,17 +1,36 @@
-
-let bookMaker = function book(title, author, read, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        if(this.read === true) {
-            console.log(`${this.title} by ${this.author}, ${this.pages} pages,  already readed!`);
-        } else {
-            console.log(`${this.title} by ${this.author}, ${this.pages} pages,  not yet readed!`);
-        }
-    }
+function hero(name, level){
+    this.name = name;
+    this.level = level;
 }
 
-const book1 = new bookMaker('The Hobbit', 'J.R.R. Tolkien', false, 295);
-book1.info();
+let hero1 = new hero('sayeed', '9th')
+console.log(hero1);
+
+hero.prototype.greet = function(){
+    return `Hello im ${this.name} and im ${this.level}'s hero!` 
+}
+
+console.log(hero1.greet());
+
+function warrior(weapon){
+    hero.call(this, name, level);
+    this.weapon = weapon;
+
+}
+warrior.prototype.attack = function(){
+    return `The warrior attacks with ${this.weapon}`
+}
+
+function healer(portion){
+ hero.call(this, name, level);
+ this.portion = portion;
+}
+
+healer.prototype.heal = function(){
+    return `The healer heals with ${this.portion} portion`
+}
+
+const hero3 = new warrior('Bjorn', 1, 'axe');
+const hero4 = new healer('Kanin', 1, 'cure');
+
+console.log(hero3);
