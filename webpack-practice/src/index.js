ea00e94653c46d0b11b18f1e5c3d135b43afea67
Sayeed <expsayeedshorif@gmail.com>
2025-01-29 10:40:42 +0600

    nothing

--- webpack-practice/src/index.js
+++ webpack-practice/src/index.js
@@ -1,9 +1,21 @@
 import "./style.css"
 import { greeting } from "./greeting.js";
+import dataXml from "./data.xml";
+import dataCsv from "./data.csv";
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 console.log(greeting);
-
-// import ginImg from "./gin.jpeg";
-const img = document.createElement('img');
-img.src = "https://cdn.create.vista.com/api/media/medium/423208882/stock-photo-cropped-view-young-woman-brown-blazer-squeezing-lemon-glass-alcohol?token=";
-document.body.appendChild(img);
+console.log(dataXml);
+console.log(dataCsv);
--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -1,6 +1,9 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
   mode: "development",
@@ -9,6 +12,10 @@ module.exports = {
     filename: "script.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -28,6 +35,35 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'], 
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
       },
     ],
   },
